19fd39e592ed4e82b5af7e1ae6bd351ef0fafd53
zhouyi <shzy2012@gmail.com>
2024-03-31 16:17:42 +0800

    .

---
+++ bge2.py
@@ -0,0 +1,11 @@
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer('BAAI/bge-base-zh-v1.5')
+
+sentences_1 = ["样例数据-1", "样例数据-2"]
+sentences_2 = ["样例数据-3", "样例数据-4"]
+model = SentenceTransformer('BAAI/bge-large-zh-v1.5')
+embeddings_1 = model.encode(sentences_1, normalize_embeddings=True)
+embeddings_2 = model.encode(sentences_2, normalize_embeddings=True)
+similarity = embeddings_1 @ embeddings_2.T
+print(similarity)
---
+++ readme.md
@@ -0,0 +1,18 @@
+## 中文embeddings example
+
+## bge vs m3e
+
+```bash
+模型大小
+
+# https://huggingface.co/BAAI/bge-base-zh-v1.5
+# bge(1.30G)
+
+
+# https://huggingface.co/moka-ai/m3e-base
+# m3e(409M)
+
+```
+
+## 向量数据库 qdrant
+
